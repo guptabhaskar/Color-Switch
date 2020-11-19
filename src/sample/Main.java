2ff2e076e00bc2ab15d64c6186d020118db8b094
Bhaskar Gupta <guptabhanu1999@gmail.com>
2020-11-19 23:48:34 +0530

    Testing 2.0

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,13 +6,18 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.awt.*;
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World A B");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Color Switch");
+        Button newG = new Button();
+        Button loadG = new Button();
+        Button exitG = new Button();
+        primaryStage.setScene(new Scene(root, 450, 700));
         primaryStage.show();
     }
 
