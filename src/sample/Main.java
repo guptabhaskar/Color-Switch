07966d49052e602fc2e507ee800161b6e53493bc
Bhaskar Gupta <guptabhanu1999@gmail.com>
2020-11-21 03:23:17 +0530

    Changed Styling

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,24 +1,19 @@
 package sample;
 
-import javafx.application.*;
-import javafx.fxml.*;
-import javafx.scene.*;
-import javafx.stage.*;
-import java.awt.*;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class Main extends Application {
-
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("MainPage.fxml"));
         primaryStage.setTitle("Color Switch");
-        Button newG = new Button();
-        Button loadG = new Button();
-        Button exitG = new Button();
         primaryStage.setScene(new Scene(root, 450, 700));
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
