2e53f1720db5b16c584d5eec60bea57b3ccfd5bb
Bhaskar Gupta <guptabhanu1999@gmail.com>
2020-11-22 18:11:59 +0530

    Main.java Updated

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -9,8 +9,10 @@ import javafx.stage.Stage;
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("MainPage.fxml"));
-//        Parent root = FXMLLoader.load(getClass().getResource("PauseGame.fxml"));
+//        Parent root = FXMLLoader.load(getClass().getResource("MainPage.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("GamePage.fxml"));
+//        Parent root = FXMLLoader.load(getClass().getResource("PausePage.fxml"));
+//        Parent root = FXMLLoader.load(getClass().getResource("ScorePage.fxml"));
         primaryStage.setTitle("Color Switch");
         primaryStage.setScene(new Scene(root, 550, 700));
         primaryStage.show();
