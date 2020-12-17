aab8600977824ced73d3ec27f5a6900415177d8a
Bhaskar Gupta <guptabhanu1999@gmail.com>
2020-12-17 17:59:09 +0530

    Minor

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,6 +10,7 @@ public class Main extends Application {
     public static Navigate Player;
     @Override
     public void start(Stage primaryStage) throws Exception{
+
         Parent root = FXMLLoader.load(getClass().getResource("MainPage.fxml"));
         primaryStage.setTitle("Color Switch");
         Scene s = new Scene(root,450,700);
